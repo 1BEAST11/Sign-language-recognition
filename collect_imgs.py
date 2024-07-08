28da053ee42a89ac199ca665589ab2907894c0df
Satyukova Kseniya <73394587+1BEAST11@users.noreply.github.com>
2024-07-08 17:45:14 +0500

    Update collect_imgs.py

--- collect_imgs.py
+++ collect_imgs.py
@@ -1,16 +1,14 @@
 import os
-
 import cv2
-
 
 DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-number_of_classes = 9
-dataset_size = 100
+number_of_classes = 9 # количество жестов для распознавания
+dataset_size = 100 # количество собираемых изображений каждого жеста
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) # окно камеры
 
 for j in range(number_of_classes):
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
@@ -32,15 +30,14 @@ for j in range(number_of_classes):
             break
 
     counter = 0
+    
     while counter < dataset_size:
         ret, frame = cap.read()
-
-        # Отзеркаливание кадра по горизонтали
-        frame = cv2.flip(frame, 1)
+        frame = cv2.flip(frame, 1) # отзеркаливание кадра по горизонтали
 
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
-        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame) # полученное изображение записывается в папку с названием counter
 
         counter += 1
 
